cff3a0cdb4d8ff3340de9b6111aa585f545e9518
codethebasics <codethebasics@gmail.com>
2022-06-22 11:07:06 -0300

    sidebar menu

--- components/layout/menu/Sidebar.js
+++ components/layout/menu/Sidebar.js
@@ -12,6 +12,33 @@ export default function Sidebar(props) {
                         <img src="/img/hamburger-gold.svg" />
                     </div>
                 </div>
+                <div className={styles.userData}>
+                    <div>
+                        <div>
+                            <span>Olá,</span>
+                            <span>Maycon Marcos</span>
+                        </div>
+                        <div className={styles.info}>
+                            maykonmarcos@gmail.com Cliente, ID# 00087
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.link}>
+                    <ul>
+                        <li>
+                            <img src="/img/home-gold.svg" height={20} />
+                            <span>Home</span>
+                        </li>
+                        <li>
+                            <img src="/img/wallet-gold.svg" height={20} />
+                            <span>Saldo</span>
+                        </li>
+                        <li>
+                            <img src="/img/signout-gold.svg" height={20} />
+                            <span>Sair</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
