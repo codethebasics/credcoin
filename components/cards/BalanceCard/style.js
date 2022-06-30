f15c0f34098f69e9ceab38e98ad44d470193b2d8
codethebasics <codethebasics@gmail.com>
2022-06-29 23:07:46 -0300

    responsivity

--- components/cards/BalanceCard/style.js
+++ components/cards/BalanceCard/style.js
@@ -8,6 +8,12 @@ export const BalanceCardComponent = styled.div`
 
 export const Icon = styled.div`
     padding: 0 15px;
+
+    @media (max-width: 1000px) {
+        img {
+            height: 30px;
+        }
+    }
 `;
 
 export const Info = styled.div`
@@ -21,6 +27,10 @@ export const Header = styled.div`
     padding: 0 15px;
     align-self: flex-end;
     font-family: gothamLight, Verdana, Geneva, Tahoma, sans-serif;
+
+    @media (max-width: 1000px) {
+        font-size: 0.7rem;
+    }
 `;
 
 export const Body = styled.div`
@@ -32,6 +42,10 @@ export const Body = styled.div`
 export const Value = styled.div`
     font-size: 1.7rem;
     padding: 0 15px;
+
+    @media (max-width: 1000px) {
+        font-size: 1.2rem;
+    }
 `;
 
 export const Symbol = styled.div`
