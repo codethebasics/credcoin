45582ce7a2c4f9adf4ed7d3ed737bb065cce8514
codethebasics <codethebasics@gmail.com>
2022-07-14 18:36:53 -0300

    auth jwt

--- pages/api/auth/index.js
+++ pages/api/auth/index.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+const $ = require('jquery');
+const jwt = require('jsonwebtoken');
 const FormData = require('form-data');
 
 export default async function authenticationHandler(req, res) {
@@ -9,29 +10,48 @@ export default async function authenticationHandler(req, res) {
         if (!username || !password) {
             res.status(400).json({ message: 'Username/Password must be sent' });
         } else {
-            const loginFormData = new FormData();
-            loginFormData.append('action', 'getDataUser');
-            loginFormData.append('chave_key', process.env.API_KEY);
-            loginFormData.append('email', username);
-            loginFormData.append('password', password);
-            await axios
-                .post(process.env.CRD_API, {
-                    method: 'POST',
-                    data: loginFormData,
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                        'Access-Control-Allow-Origin': '*',
-                        'Content-Type': 'application/json',
-                    },
-                })
-                .then((response) => {
-                    console.log(response);
-                    res.status(200).json({ message: 'ok' });
-                })
-                .catch((error) => {
-                    console.log(error);
-                    res.status(500).json({ message: 'Authentication error.' });
-                });
+            const form = new FormData();
+            form.append('action', 'getDataUser');
+            form.append('chave_key', process.env.API_KEY);
+            form.append('email', username);
+            form.append('password', password);
+
+            const response = await fetch(process.env.CRD_API, {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'default',
+                body: form,
+            });
+
+            if (response) {
+                const jsonResponse = await response.json();
+                const user = jsonResponse.data[0];
+                const KEY = process.env.API_SECRET;
+
+                if (user) {
+                    const token = jwt.sign(
+                        {
+                            name: user.name,
+                            email: user.email,
+                            level_id: user.level_id,
+                            document_user: user.document_user,
+                            uf: user.uf,
+                            city: user.city,
+                            ip_conect: user.ip_conect,
+                        },
+                        KEY
+                    );
+
+                    res.status(200).json({
+                        token: token,
+                        user: user,
+                    });
+                } else {
+                    res.status(400).json({
+                        message: 'Authentication failed.',
+                    });
+                }
+            }
         }
     } else {
         res.status(500).json({ message: 'Method Not Allowed.' });
