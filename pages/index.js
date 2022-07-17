9c869808b369c807930baa1d59e886a3795b6c19
codethebasics <codethebasics@gmail.com>
2022-07-17 20:57:05 -0300

    refresh

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Router from 'next/router';
 
 export default function Index(props) {
     useEffect(() => {
+        // redirect
         props.setPaths(['Home']);
         Router.push('/home');
     }, []);
