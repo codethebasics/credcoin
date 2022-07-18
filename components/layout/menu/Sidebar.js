ba40b5b1cc58fa24e28f2e77c645fbea128433b3
codethebasics <codethebasics@gmail.com>
2022-07-18 11:32:51 -0300

    fix logo

--- components/layout/menu/Sidebar.js
+++ components/layout/menu/Sidebar.js
@@ -26,7 +26,7 @@ export default function Sidebar(props) {
                     <ul>
                         <Link href="/home">
                             <li>
-                                <img src="/img/logo_CRD.png" height={20} />
+                                <img src="/img/home-gold.svg" height={20} />
                                 <a>Home</a>
                             </li>
                         </Link>
